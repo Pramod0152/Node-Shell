8fb7c8cfc9e32ef7751cbaf00b0cb0b52d1cfb07
Pramod Ghimire <pramodghimire96@gmail.com>
2025-11-18 20:53:12 +0545

    Implement Exit

--- app/main.js
+++ app/main.js
@@ -8,6 +8,10 @@ const rl = readline.createInterface({
 // TODO: Uncomment the code below to pass the first stage
 function prompt() {
   rl.question("$ ", (command) => {
+    if (command.trim() === "exit 0") {
+      rl.close();
+      process.exit(0);
+    }
     console.log(`${command}: command not found`);
     prompt(); // call next prompt AFTER user input
   });
