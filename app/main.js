2421ea2e00d3d7ab49867dde5766cf09d4762526
Pramod Ghimire <pramodghimire96@gmail.com>
2025-11-21 22:23:28 +0545

    Refactored codebase

--- app/main.js
+++ app/main.js
@@ -6,34 +6,66 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const builtInCommands = {
-  type: "type",
-  echo: "echo",
-  exit: "exit",
-};
+function exitcmd(...args) {
+  rl.close();
+  process.exit(0);
+}
 
+function echocmd(...args) {
+  console.log(args.join(" "));
+}
+
+function typecmd(...args) {
+  if (args[0] === undefined) {
+    console.log("type: missing argument");
+  } else if (args[0] in builtInCommands) {
+    console.log(`${args[0]} is a shell builtin`);
+  } else {
+    console.log(`${args[0]}: not found`);
+  }
+}
+
+const builtInCommands = {
+  type: typecmd,
+  echo: echocmd,
+  exit: exitcmd,
+};
 // TODO: Uncomment the code below to pass the first stage
-function prompt() {
+// function prompt() {
+//   rl.question("$ ", (command) => {
+//     const trimmedCommand = command.trim().split(" ");
+//     const first = trimmedCommand[0];
+//     const args = trimmedCommand.slice(1).join(" ");
+//     if (first === "type") {
+//       if (builtInCommands[args]) {
+//         console.log(`${args} is a shell builtin`);
+//       } else {
+//         console.log(`${args}: not found`);
+//       }
+//     } else if (first === "echo") {
+//       console.log(args);
+//     } else if (first === "exit") {
+//       rl.close();
+//       process.exit(0);
+//     } else {
+//       console.log(`${command}: command not found`);
+//     }
+//     prompt();
+//   });
+// }
+
+// prompt();
+
+function REPL() {
   rl.question("$ ", (command) => {
-    const trimmedCommand = command.trim().split(" ");
-    const first = trimmedCommand[0];
-    const args = trimmedCommand.slice(1).join(" ");
-    if (first === "type") {
-      if (builtInCommands[args]) {
-        console.log(`${args} is a shell builtin`);
-      } else {
-        console.log(`${args}: not found`);
-      }
-    } else if (first === "echo") {
-      console.log(args);
-    } else if (first === "exit") {
-      rl.close();
-      process.exit(0);
+    const answer = command.split(" ");
+    if (answer[0] in builtInCommands) {
+      builtInCommands[answer[0]](...answer.slice(1));
     } else {
       console.log(`${command}: command not found`);
     }
-    prompt();
+    REPL();
   });
 }
 
-prompt();
+REPL();
