870192423e7396e445c108897cc64dbf16b0de81
Pramod Ghimire <pramodghimire96@gmail.com>
2025-11-22 09:49:52 +0545

    Implement Basic Executer

--- app/main.js
+++ app/main.js
@@ -1,5 +1,8 @@
 const { exit } = require("process");
 const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
+const { spawn } = require("child_process");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -21,6 +24,22 @@ function typecmd(...args) {
   } else if (args[0] in builtInCommands) {
     console.log(`${args[0]} is a shell builtin`);
   } else {
+    const argPath = args[0];
+    const paths = process.env.PATH.split(path.delimiter);
+    for (const dir of paths) {
+      const filePath = path.join(dir, argPath);
+      try {
+        const stats = fs.statSync(filePath);
+        if (!stats.isFile()) {
+          continue;
+        }
+        fs.accessSync(filePath, fs.constants.X_OK);
+        console.log(`${args[0]} is ${filePath}`);
+        return;
+      } catch (err) {
+        continue;
+      }
+    }
     console.log(`${args[0]}: not found`);
   }
 }
@@ -30,41 +49,28 @@ const builtInCommands = {
   echo: echocmd,
   exit: exitcmd,
 };
-// TODO: Uncomment the code below to pass the first stage
-// function prompt() {
-//   rl.question("$ ", (command) => {
-//     const trimmedCommand = command.trim().split(" ");
-//     const first = trimmedCommand[0];
-//     const args = trimmedCommand.slice(1).join(" ");
-//     if (first === "type") {
-//       if (builtInCommands[args]) {
-//         console.log(`${args} is a shell builtin`);
-//       } else {
-//         console.log(`${args}: not found`);
-//       }
-//     } else if (first === "echo") {
-//       console.log(args);
-//     } else if (first === "exit") {
-//       rl.close();
-//       process.exit(0);
-//     } else {
-//       console.log(`${command}: command not found`);
-//     }
-//     prompt();
-//   });
-// }
-
-// prompt();
 
 function REPL() {
   rl.question("$ ", (command) => {
     const answer = command.split(" ");
     if (answer[0] in builtInCommands) {
       builtInCommands[answer[0]](...answer.slice(1));
+      return REPL();
     } else {
-      console.log(`${command}: command not found`);
+      const child = spawn(answer[0], answer.slice(1));
+      child.stdout.on("data", (data) => {
+        process.stdout.write(data);
+      });
+
+      child.on("error", () => {
+        console.log(`${answer[0]}: command not found`);
+        return REPL();
+      });
+
+      child.on("close", () => {
+        return REPL();
+      });
     }
-    REPL();
   });
 }
 
